0931b9322441d3effbd5fa8de01f04dedf698225
FoxSide <foxside8452@gmail.com>
2021-11-01 15:25:59 +0200

    01.11

---
+++ 8 kyu Counting sheep....js
@@ -0,0 +1,13 @@
+// function countSheeps(arrayOfSheep) {
+//   let count = 0
+//   for (let i = 0; i < arrayOfSheep.length; i++) {
+//     if (arrayOfSheep[i] == true)
+//       count ++
+//   }
+//   return count
+// }
+
+function countSheeps(arrayOfSheep) {
+  let count = arrayOfSheep.filter((a => a)).length
+  return count
+}
