fe1cdeefdfc7545f69adb719d4171eb4f2b32225
FoxSide <foxside8452@gmail.com>
2021-11-04 15:59:54 +0200

    04.11

--- 6 kyu Are they the same.js
+++ 6 kyu Are they the same.js
@@ -1,4 +1,11 @@
-function comp(array1, array2){
-  array1=array1.map(v=>v*v)
-  return array2?array1.reduce((a,b)=>a+b,0)===array2.reduce((a,b)=>a+b,0):false
+function comp(array1, array2) {
+  if (!array1 || !array2) return false;
+  array1 = array1.map(a => { return a * a }).sort((a, b) => { return b - a });
+  array2 = array2.sort((a, b) => { return b - a });
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
